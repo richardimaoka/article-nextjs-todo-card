a111911ad840dead13ab74fda95eed82142ec242
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-03-26 04:54:20 +0900

    page by todoId

--- next/app/items/[todoId]/page.tsx
+++ next/app/items/[todoId]/page.tsx
@@ -3,8 +3,14 @@ import { ServerActionWrapper } from "@/app/components/ServerActionWrapper";
 import { TodoCard } from "@/app/components/card/TodoCard";
 import styles from "./page.module.css";
 
-export default async function Page() {
-  const res = await fetch("http://localhost:3036/items/a");
+interface Props {
+  params: {
+    todoId: string;
+  };
+}
+
+export default async function Page(props: Props) {
+  const res = await fetch(`http://localhost:3036/items/${props.params.todoId}`);
   const item: TodoItem = await res.json();
 
   return (
